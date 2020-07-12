e3df1b3931c7b22df876353f99dc5c4f1060fd2d
Ilia Selieznov <ziziron21@gmail.com>
2020-07-13 00:28:01 +0300

    Ilya to master -> css, js -> slider(nav)

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let headerHome = document.querySelector('.header__home'),
+    headerAboutUs = document.querySelector('.header__aboutUs'),
+    headerVacancies = document.querySelector('.header__vacancies'),
+    headerContacts = document.querySelector('.header__contacts');
+let headerHomeM = document.querySelector('.header__homeM'),
+    headerAboutUsM = document.querySelector('.header__aboutUsM'),
+    headerVacanciesM = document.querySelector('.header__vacanciesM'),
+    headerContactsM = document.querySelector('.header__contactsM');
+let arrowButtonNext = document.querySelector('.footer__img');
+
