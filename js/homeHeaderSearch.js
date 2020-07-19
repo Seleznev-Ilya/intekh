099457b57cddeaa6c28a119aac49108aa48830c0
Ilia Selieznov <ziziron21@gmail.com>
2020-07-19 20:02:06 +0300

    search bar

---
+++ js/homeHeaderSearch.js
@@ -0,0 +1,35 @@
+let headerSearch = document.querySelector('.header__search');
+let headerSearchContainer = document.querySelector('.header__search-container');
+let headerSearchInput = document.querySelector('.header__search-input');
+let headerButtonSearch = document.querySelector('.header__button-search');
+let headerSearchImg = document.querySelector('.header__search-img');
+let headerSearchP = document.querySelector('.header__search-p');
+let headerSearchSuggest = document.querySelector('.header__search-suggest');
+
+headerButtonSearch.addEventListener("click", () => {
+    headerSearch.classList.toggle('header__active-headerSearch');
+    headerButtonSearch.classList.toggle('header__active-headerButtonSearch');
+    headerSearchImg.classList.toggle('header__hidden');
+    headerSearchP.classList.toggle('header__hidden');
+    headerSearchInput.classList.toggle('header__hidden');
+    headerSearchInput.value = '';
+
+});
+headerSearchInput.onfocus = function () {
+    headerSearchSuggest.style.display = 'block';
+    headerSearchSuggest.style.opacity = '1';
+    headerSearchSuggest.style.transform = 'translateY(4px)';
+};
+headerSearchInput.onblur = function () {
+    headerSearchSuggest.style.transform = 'translateY(-49px)';
+    headerSearchSuggest.style.opacity = '0';
+    setTimeout(() => headerSearchSuggest.style.display = 'none', 100);
+    if (headerSearchInput.value === '') {
+        console.log(headerSearchInput.value, 'headerSearchInput.value');
+        headerSearch.classList.toggle('header__active-headerSearch');
+        headerButtonSearch.classList.toggle('header__active-headerButtonSearch');
+        headerSearchImg.classList.toggle('header__hidden');
+        headerSearchP.classList.toggle('header__hidden');
+        headerSearchInput.classList.toggle('header__hidden');
+    }
+};
