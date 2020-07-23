7dfaa1a80500a53f86fdb4296083a1af5e8fcc17
Dan <danyaslik26@gmail.com>
2020-07-23 17:10:27 +0300

    23.07.2020

--- js/home.js
+++ js/home.js
@@ -84,3 +84,23 @@ function h1Text() {
 }
 
 h1Text();
+
+/* SubMenu */
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
